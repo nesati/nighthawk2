e717c356d1ab8d8673ab8506686994666a48cb5a
kokolem <vitek.peterk@gmail.com>
2022-02-06 11:28:07 +0100

    Redirect to admin site after login

--- nighthawk2/settings.py
+++ nighthawk2/settings.py
@@ -125,6 +125,10 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = 'media'
 MEDIA_URL = '/media/'
 
+# Redirect to admin site after login
+
+LOGIN_REDIRECT_URL = '/admin'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
