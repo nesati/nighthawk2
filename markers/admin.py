626fa7f6a0482cdeea0491d0b47e50ef48349a2b
kokolem <vitek.peterk@gmail.com>
2022-02-06 12:18:07 +0100

    Add years and accept button to markers overview

--- markers/admin.py
+++ markers/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from nighthawk2 import settings
 from .models import AcceptedMarker, MarkerProposal, Image
@@ -14,10 +15,38 @@ class ImageInline(admin.TabularInline):
 
 
 class MarkerAdmin(admin.ModelAdmin):
+    def captured_in_years(self, obj):
+        images = Image.objects.filter(marker=obj).order_by('year')
+        return ', '.join([str(image.year) for image in images])
+
+    captured_in_years.short_description = "v letech"
+
+    list_display = (
+        '__str__',
+        'captured_in_years'
+    )
+
     inlines = [
         ImageInline,
     ]
 
 
+class MarkerProposalAdmin(MarkerAdmin):
+    def changelist_view(self, request, extra_context=None):
+        default = MarkerAdmin.list_display
+        if request.user.has_perm('markers.accept_markerproposal'):
+            self.list_display = default + (
+                'accept_button',
+            )
+        else:
+            self.list_display = default
+        return super().changelist_view(request, extra_context)
+
+    def accept_button(self, obj):
+        return format_html('<a class="button" href="#" style="text-decoration: none">Přijmout</a>')
+
+    accept_button.short_description = 'možnosti'
+
+
 admin.site.register(AcceptedMarker, MarkerAdmin)
-admin.site.register(MarkerProposal, MarkerAdmin)
+admin.site.register(MarkerProposal, MarkerProposalAdmin)
